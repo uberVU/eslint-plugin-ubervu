8fb2da07147440b21bd775ab09e00496a9668d2a
Flavius Tirnacop <flavius.tirnacop@hootsuite.com>
2015-10-02 15:31:24 +0300

    Add skeleton files #1

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * @fileoverview ESLint rules applicable to the uberVU org
+ * @author uberVU
+ * @copyright 2015 uberVU. All rights reserved.
+ * See LICENSE file in root directory for full license.
+ */
+'use strict';
+
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+
+
+// import all rules in lib/rules
+module.exports = {
+  rules: {
+  },
+  rulesConfig: {
+  }
+};
