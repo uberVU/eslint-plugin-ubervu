5d3c20cba26e3f4ca6a999afe0af95094a2692c5
Flavius Tirnacop <flavius.tirnacop@hootsuite.com>
2015-10-08 16:56:32 +0300

    Fix indent-function-args test file sytling #2

--- test/lib/rules/indent-function-args.js
+++ test/lib/rules/indent-function-args.js
@@ -6,85 +6,60 @@
  */
 'use strict';
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
 var rule = require('../../../lib/rules/indent-function-args'),
-
     RuleTester = require('eslint').RuleTester;
-
-
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester();
 ruleTester.run('indent-function-args', rule, {
-
-  valid: [
-    {
-      code: 'foo(arg1, arg2);'
-    },
-    {
-      code: 'foobar(arg1,\n' +
-            '    arg2);'
-    },
-    {
-      code: 'foo(arg1,\n' +
-            '    arg2);'
-    },
-    {
-      code: 'foobar(arg1,\n' +
-            '    arg2,\n' +
-            '    arg3);'
-    },
-    {
-      code: 'foobar(arg1,\n' +
-            '       arg2,\n' +
-            '       arg3);'
-    },
-    {
-      code: 'foobar(arg1,\n' +
-            '       arg2, arg3,\n' +
-            '       arg4,\n' +
-            '       arg5);'
-    },
-    {
-      code: 'var foo = bar(arg1, arg2,\n' +
-            '    arg3, arg4);'
-    },
-    {
-      code: 'foo({\n' +
-            '  a: 2,\n' +
-            '  b: 3\n' +
-            '}, arg2);'
-    }
-  ],
-
-  invalid: [
-    {
-      code: 'foobar(arg1,\n' +
-            '    arg2,\n' +
-            '       arg3);',
-      errors: [{
-        message: 'Indent at 4 spaces.'
-      }]
-    },
-    {
-      code: 'foobar(arg1,\n' +
-            '       arg2,\n' +
-            '    arg3);',
-      errors: [{
-        message: 'Indent at parent parenthesis.'
-      }]
-    },
-    {
-      code: 'var foo = bar(arg1, arg2,\n' +
-            'arg3, arg4);',
-      errors: [{
-        message: 'Indent at 4 spaces or paren.'
-      }]
-    }
-  ]
+  valid: [{
+    code: 'foo(arg1, arg2);'
+  }, {
+    code: 'foobar(arg1,\n' +
+          '    arg2);'
+  }, {
+    code: 'foo(arg1,\n' +
+          '    arg2);'
+  }, {
+    code: 'foobar(arg1,\n' +
+          '    arg2,\n' +
+          '    arg3);'
+  }, {
+    code: 'foobar(arg1,\n' +
+          '       arg2,\n' +
+          '       arg3);'
+  }, {
+    code: 'foobar(arg1,\n' +
+          '       arg2, arg3,\n' +
+          '       arg4,\n' +
+          '       arg5);'
+  }, {
+    code: 'var foo = bar(arg1, arg2,\n' +
+          '    arg3, arg4);'
+  }, {
+    code: 'foo({\n' +
+          '  a: 2,\n' +
+          '  b: 3\n' +
+          '}, arg2);'
+  }],
+  invalid: [{
+    code: 'foobar(arg1,\n' +
+          '    arg2,\n' +
+          '       arg3);',
+    errors: [{
+      message: 'Indent at 4 spaces.'
+    }]
+  }, {
+    code: 'foobar(arg1,\n' +
+          '       arg2,\n' +
+          '    arg3);',
+    errors: [{
+      message: 'Indent at parent parenthesis.'
+    }]
+  }, {
+    code: 'var foo = bar(arg1, arg2,\n' +
+          'arg3, arg4);',
+    errors: [{
+      message: 'Indent at 4 spaces or paren.'
+    }]
+  }]
 });
